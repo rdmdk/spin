072948ddc8204bfab13d9eca43f8461cb3819984
Redmond <rdmdk@users.noreply.github.com>
2020-01-16 13:00:40 +0000

    Update spin.js

--- spin.js
+++ spin.js
@@ -12,20 +12,13 @@ list.sort(() => Math.random() - 0.5);
 
 function spacing() {
 	var l = list.length;
-	/*
 	var f1, f2;
-	if (l >= 40) f1 = 15, f2 = 0.1;
-	else if (l >= 35) f1 = 14, f2 = 0.15;
-	else if (l >= 30) f1 = 13, f2 = 0.2; 
-	else if (l >= 25) f1 = 12, f2 = 0.25; 
-	else if (l >= 20) f1 = 11, f2 = 0.3; 
-	else if (l >= 15) f1 = 9.5, f2 = 0.5; 
-	else if (l >= 10) f1 = 9, f2 = 0.6; 
-	else if (l >= 5) f1 = 8, f2 = 0.7; 
+	if (l >= 45) f1 = 10, f2 = 0.1;
+	else if (l >= 30) f1 = 10, f2 = 0.2;
+	else if (l >= 15) f1 = 10, f2 = 0.5;
 	var s = "main{font-size:" + f1 + "rem}span{font-size:" + f2 + "em}";
 	if (document.head.querySelector("style")) document.head.querySelector("style").innerText = s;
 	else document.head.insertAdjacentHTML("beforeend", "<style>" + s + "</style>");
-	*/
 }
 
 function update() {
