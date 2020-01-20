e3c58c24ea4ac75b25a70bff7c8a4e372b8d7b23
Redmond <rdmdk@users.noreply.github.com>
2020-01-20 13:19:14 +0000

    Update spin.js

--- spin.js
+++ spin.js
@@ -29,7 +29,7 @@ function update() {
 				list += i + ",";
 			}
 		} else if (input === "abc") list = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
-		else if (input === "ci") list = "ac,alg,ba,da,dl,jl,li,mf,mt,on,rd,sl,sw,vz";
+		else if (input === "ci") list = "ac,alg,b,da,dl,j,l,mf,mt,o,p,r,sl,sw,v";
 		else list = input;
 		list = list.replace(/^\,|\,$/gm, "").replace(/\,+/gm, ",");
 		localStorage.list = list;
