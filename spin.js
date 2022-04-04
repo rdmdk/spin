8be709be4d15e4ecf7c357dbeb81cfbbc0b1d5d1
Redmond <rdmdk@users.noreply.github.com>
2022-04-04 16:27:58 +0100

    Update spin.js

--- spin.js
+++ spin.js
@@ -36,7 +36,7 @@ function update() {
 				list += i + ',';
 			}
 		} else if (input === 'abc') list = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z';
-		else if (input === 'ci') list = 'ac,ba,bc,cb,jp,li,mf,mo,on,pw,rd,vz';
+		else if (input === 'ci') list = 'cb,jp,li,mf,mo,on,pw,rd,sh,vz';
 		else list = input;
 		list = list.replace(/^\,|\,$/gm, '').replace(/\,+/gm, ',');
 		localStorage.list = list;
