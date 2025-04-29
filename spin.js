8cd23b370acb34576414f366125c86a3cbaee2cb
Redmond <rdmdk@users.noreply.github.com>
2025-04-29 09:32:43 +0100

    Update spin.js

--- spin.js
+++ spin.js
@@ -36,7 +36,7 @@ function update(x) {
 		} else if (input === 'abc') list = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z';
 		else if (/ci[_ab]*$/gm.test(input)) {
 			const ci_a = 'ls,mf,mq,pl,pw,rh,rs,so';
-			const ci_b = 'dd,fg,jw,ho,rt,wg';
+			const ci_b = 'dd,fg,jw,ho,wg';
 			const ci_c = 'ab,on,rd,vz';
 			list = (input === 'ci_a' ? ci_a : input === 'ci_b' ? ci_b : ci_a + ',' + ci_b) + ',' + ci_c;
 		}
