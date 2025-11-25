68d3cb2dc8e750698b2727f6e97a8f8477712713
Redmond <rdmdk@users.noreply.github.com>
2025-11-25 10:22:31 +0000

    Update spin.js

--- spin.js
+++ spin.js
@@ -1,13 +1,14 @@
-var main = document.querySelector('main');
-var section = document.querySelector('section');
-var button = document.querySelector('button');
-var button_span = button.querySelector('span');
-var edit = document.querySelector('.edit');
-var colors = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'];
-var list;
+const main = document.querySelector('main'),
+section = document.querySelector('section'),
+button = document.querySelector('button'),
+button_span = button.querySelector('span'),
+edit = document.querySelector('.edit'),
+colors = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'];
+
+let list;
 
 if ((window.location.hash && window.location.hash !== '#') || (window.location.search && window.location.search !== '?')) {
-	var h_s = window.location.hash || window.location.search;
+	const h_s = window.location.hash || window.location.search;
 	update(decodeURI(h_s.substring(1)));
 	if (window.location.hash) window.location.hash = '';
 	if (window.location.search) window.location.search = '';
@@ -15,7 +16,7 @@ if ((window.location.hash && window.location.hash !== '#') || (window.location.s
 else update();
 
 function spacing() {
-	var l = list.length,
+	const l = list.length,
 	fs1 = l <= 15 ? 9 : l <= 30 ? 10.5 : l <= 45 ? 11.5 : 12,
 	fs2 = l <= 15 ? 0.4 : l <= 30 ? 0.15 : l <= 45 ? 0.1 : l <= 60 ? 0.075 : l <= 75 ? 0.04 : 0.04,
 	s = 'main{font-size:' + fs1 + 'rem}span{font-size:' + fs2 + 'em}';
@@ -25,10 +26,10 @@ function spacing() {
 }
 
 function update(x) {
-	var input = x ? x : window.prompt('Enter a comma-delimited list of initials', localStorage.list);
+	const input = x ? x : window.prompt('Enter a comma-delimited list of initials', localStorage.list);
 	if (input !== '' && input !== null) {
 		if (input.match(/^[0-9]+$/gm)) {
-			var n = parseInt(input);
+			const n = parseInt(input);
 			list = '';
 		    	for (var i = 1; i <= n; i++) {
 				list += i + ',';
@@ -61,7 +62,7 @@ function setup(a) {
 
 function shifting(a) {
 	for (let i = 0; i < a; i++) {
-		var b = list.shift();
+		const b = list.shift();
 		list.push(b);
 	}
 	setup();
@@ -108,6 +109,6 @@ window.addEventListener('keyup', (e) => {
 	}
 });
 
-var c = Math.floor(Math.random() * colors.length);
+const c = Math.floor(Math.random() * colors.length);
 section.classList.add(colors[c]);
 edit.classList.add(colors[c]);
